db4da52ebbab1bdd8f6d2a98307e0bb4e4035b32
Remigijus Jarmalavičius <remigijus@jarmalavicius.lt>
2014-03-30 11:58:52 +0300

    Mysql will make dumps without increasing CPU usage

--- backup_mysql.sh
+++ backup_mysql.sh
@@ -10,5 +10,5 @@ mkdir -p $directory/$2/$date
 databases=$(ssh $1 "mysql -N -u root -p$3 -e 'SHOW DATABASES'" | grep -v 'information_schema')
 for db in $databases
 do
-  ssh $1 "mysqldump --force --opt -uroot -p$3 --databases $db" > $directory/$2/$date/$db.sql
+  ssh $1 "mysqldump --single-transaction --quick --lock-tables=false --force --opt -uroot -p$3 --databases $db" > $directory/$2/$date/$db.sql
 done
